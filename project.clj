91928bafa18c1174c34964ab3db04d0c8fb4c01a
diavoletto76 <diavoletto76@gmail.com>
2019-06-28 00:16:08 +0200

    Adds details to lein projects file.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject mbox-parser "0.1.0-SNAPSHOT"
-  :description "Pure Clojure implementation of lazy Mbox parser"
-  :url "http://example.com/FIXME"
+(defproject org.diavoletto76/mbox-parser "0.1.0-SNAPSHOT"
+  :description "Pure Clojure implementation of a lazy Mbox parser"
+  :url "https://github.com/diavoletto76/mbox-parser"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
